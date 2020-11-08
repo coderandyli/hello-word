5354fa29e921dfbf36eb1565c57585fa73b715ba
coderandyli <coderandyli@163.com>
2020-11-08 21:22:55 +0800

    no message

---
+++ src/main/java/com/coderandyli/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.coderandyli.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author lizhenzhen
+ * @version 1.0
+ * @date 2020/11/8 下午9:15
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/name")
+    public String hello() {
+        return "my name is andy";
+    }
+}
